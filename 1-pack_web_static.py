58b9ba9bfc11985c817d0a4e3fed332198a46afb
marcelinus <marcelinusilonuba21@gmail.com>
2024-05-19 16:37:36 -0700

    task1

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 """
-a Fabric script that generates a .tgz archive from the contents of the web_staticfolder of your AirBnB Clone repo, using the function do_pack
+a Fabric script that generates a .tgz archive from the contents of the web_staticf
+older of your AirBnB Clone repo, using the function do_pack
+"""
+from fabric.api import *
+from datetime import datetime
+from os.path import isdir
+import os
+
+
+def do_pack():
+
+
+    """ A sceript that generates a .tgz archive"""
+
+    try:
+        if isdir('versions') is false:
+            local('sudo mkdir -p versions')
+        now = datetime.now()
+        t_now = now.strftime('%Y%m%d%H%M%S')
+        local(f'sudo tar -cvzf versions/web_static_{t_now}.tgz web_static')
+        f_path = f'versions/web_static_{t_now}.tgz'
+        f_size = os.path.getsize(f_path)
+        print(f'web_static packed: versions/web_static_{t_now}.tgz -> {f_size}Bytes')
+        return f_path
+    except:
+        return None
