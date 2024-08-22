e3c027bc607ff524f51b7955ba5ddd6f856b65af
root <root@DESKTOP-1GU0KQ2.localdomain>
2024-08-22 13:14:48 +0100

    flask

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""start a flem web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/airbnb-onepage', strict_slashes=False)
+def home():
+    """display helloworld"""
+    return "Hello HBNB!"
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0', port=5000)
